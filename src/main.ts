fb0371b4f25da8371a1b2cf59aa76d9eca901f8f
Kim <kohkimkhang1@gmail.com>
2025-01-24 17:57:00 +0800

    g

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors();
+
   // Enable global validation
   app.useGlobalPipes(new ValidationPipe());
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
